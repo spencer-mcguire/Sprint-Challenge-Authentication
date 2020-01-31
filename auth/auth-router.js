726dab1f5f173bd90c9eec43232ba2db2702c87a
spencer-mcguire <smcguire1826@gmail.com>
2020-01-31 10:26:04 -0600

    user-model

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,4 +1,22 @@
 const router = require('express').Router();
+const Users = require('../database/helpers/users-model');
+
+router.get('/users', (req, res) => {
+  Users.find()
+    .then(users => {
+      if (users.length > 0) {
+        res.json(users);
+      } else {
+        res.status(404).json({ error_message: 'no users found' });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: err, error_message: 'Something happend..' });
+    });
+});
 
 router.post('/register', (req, res) => {
   // implement registration
